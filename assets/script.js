210487cc399205c31c6c2451f136f48b4b75d24c
Connor S <constroh@gmail.com>
2023-05-03 11:18:27 -0500

    time slot function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 $(function () {
+  currentDate();
   timeSlot();
 });
 
@@ -6,4 +7,20 @@ $(function () {
 function currentDate() {
   const currentDay = dayjs();
   $("#currentDay").text(currentDay.format("dddd, MMMM D YYYY"));
+}
+
+// Time slot function for past, present, future.
+function timeSlot() {
+  const currentHour = dayjs().hour();
+  const timeBlock = $(".time-block");
+  timeBlock.each(function () {
+    let hour = $(this).attr("id");
+    if (hour == currentHour) {
+      $(this).addClass("present");
+    } else if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour > currentHour) {
+      $(this).addClass("future");
+    }
+  });
 }
